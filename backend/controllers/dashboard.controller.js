9e0e8e606ab96cf08b8bc6d781c0aa9cbd007ce7
tranngocthach <tranngocthach0919@gmail.com>
2023-09-09 19:48:21 +0700

    create api dashboard for backend and get data dashboard page admin test 2

--- backend/controllers/dashboard.controller.js
+++ backend/controllers/dashboard.controller.js
@@ -2,16 +2,18 @@ const { customers, orders } = require("../shared/shared");
 
 class DashboardController {
   getDataDashboard(req, res) {
+    let dataOrders = orders;
 
     let totalUsers = customers.length;
-    // let totalProfit = orders.products.reduce((acc, products) => {
-    //   return acc += (products.quantity * (products.salePrice - products.entryPrice));
-    // }, 0)
-    let totalOrders = orders.length;
+    let totalProfit = dataOrders.products.reduce((acc, products) => {
+      return acc += (products.quantity * (products.salePrice - products.entryPrice));
+    }, 0)
+    let totalOrders = dataOrders.length;
     console.log(totalUsers);
     console.log(totalOrders);
+    console.log(totalProfit);
 
-    res.json({ totalUsers, totalProfit: 45000000, totalOrders });
+    res.json({ totalUsers, totalProfit, totalOrders });
 
   };
 }

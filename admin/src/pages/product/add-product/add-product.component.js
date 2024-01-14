6be89f66d0754f62e600a25598631cbe45794d13
tranngocthach0919 <113542074+tranngocthach0919@users.noreply.github.com>
2024-01-14 19:25:39 +0700

    Update add-product.component.js

--- admin/src/pages/product/add-product/add-product.component.js
+++ admin/src/pages/product/add-product/add-product.component.js
@@ -36,7 +36,7 @@ const AddProduct = () => {
                         <div className="bg-white rounded shadow-lg p-4 px-4 md:p-8 mb-6">
                             <div className="grid gap-4 gap-y-2 text-sm grid-cols-1 lg:grid-cols-3">
                                 <div className="text-gray-600">
-                                    <p className="font-medium text-lg">Add Employee</p>
+                                    <p className="font-medium text-lg">Add Product</p>
                                     <p>Please fill out all the fields.</p>
                                 </div>
 
